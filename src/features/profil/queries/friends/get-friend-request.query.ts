948b0f4d3a593447ad656666f377792a23da1f51
Amir <76406446+amir-398@users.noreply.github.com>
2026-03-05 13:09:48 +0100

    fix(SCRUM-452): Add GET method error tracking, ignoring 404s, and disable retries for the friend request query. (#28)

--- src/features/profil/queries/friends/get-friend-request.query.ts
+++ src/features/profil/queries/friends/get-friend-request.query.ts
@@ -1,13 +1,25 @@
 import { useFriendsFindMyFriendRequest } from '@generatedApi/friends/friends.api';
 
+import { ErrorResponse } from '@/api/orval.instance';
+import { useGetMethodErrorTracking } from '@/hooks/analytics-trackers.hook';
+
 export const useGetFriendRequest = (userUid?: string) => {
   const query = useFriendsFindMyFriendRequest(userUid, {
     query: {
       enabled: !!userUid,
+      retry: false,
     },
   });
 
   const { data } = query?.data || {};
+
+  const { error, isError } = query || {};
+
+  const erroResponse = error as unknown as ErrorResponse;
+
+  const is404 = erroResponse?.api_error?.statusCode === 404 || erroResponse?.api_error_status === 404;
+
+  useGetMethodErrorTracking({ error, isError: isError && !is404 });
 
   return {
     ...query,

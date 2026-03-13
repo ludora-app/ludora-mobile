60159715d9870722677aa281f6206021bd567239
Amir <76406446+amir-398@users.noreply.github.com>
2026-03-13 19:23:48 +0100

    feat(SCRUM-532): Introduce a dedicated staging API environment and URL configuration. (#57)

--- src/utils/api-url.utils.ts
+++ src/utils/api-url.utils.ts
@@ -1,5 +1,6 @@
 const LOCALHOST_API_URL = `http://${process.env.EXPO_PUBLIC_IP_ADDRESS}:2424`;
 const DEV_API_URL = process.env.EXPO_PUBLIC_DEV_API_URL;
+const STAGING_API_URL = process.env.EXPO_PUBLIC_STAGING_API_URL;
 const PROD_API_URL = process.env.EXPO_PUBLIC_PROD_API_URL;
 
 const env = process.env.EXPO_PUBLIC_API_ENV || 'production';
@@ -11,8 +12,11 @@ export const getApiUrl = () => {
   if (env === 'localhost') {
     return LOCALHOST_API_URL;
   }
-  if (env === 'development' || env === 'preview') {
+  if (env === 'development') {
     return DEV_API_URL;
+  }
+  if (env === 'preview') {
+    return STAGING_API_URL;
   }
   return PROD_API_URL;
 };

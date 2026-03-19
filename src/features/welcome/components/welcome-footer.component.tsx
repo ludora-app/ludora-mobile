943ff46269e1d297c0078c1ccb27af69bbdfdc19
Amir <76406446+amir-398@users.noreply.github.com>
2026-03-19 17:00:14 +0100

    fix(SCRUM-559): Update button titles in WelcomeFooter component for consistency with common translations (#87)

--- src/features/welcome/components/welcome-footer.component.tsx
+++ src/features/welcome/components/welcome-footer.component.tsx
@@ -6,9 +6,9 @@ export default function WelcomeFooter() {
 
   return (
     <Wrapper className="mb-10 w-full gap-4">
-      <Button title={t('welcome.footer_button_start_game')} size="xl" redirect="/auth/login" />
+      <Button title={t('common.login')} size="xl" redirect="/auth/login" />
       <Button
-        title={t('welcome.footer_button_join_game')}
+        title={t('common.register')}
         variant="outlined"
         redirect="/auth/register/step-1"
         size="xl"

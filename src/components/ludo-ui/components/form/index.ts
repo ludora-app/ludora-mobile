cd9508d885072ac03e789226674eb1a627be905f
amir-398 <amir.398@hotmail.fr>
2026-03-22 20:13:12 +0100

    feat(SCRUM-567): update input cursor and selection colors.

--- src/components/ludo-ui/components/form/index.ts
+++ src/components/ludo-ui/components/form/index.ts
@@ -1,3 +1,2 @@
 export { default as FormInput } from './form-input.component';
-export { default as FormInputGestureHandler } from './form-input-gesture-handler.component';
 export { default as FormDatePickerInput } from './form-date-picker-input.component';

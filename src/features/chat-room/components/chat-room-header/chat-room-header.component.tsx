554a062ede2b2fadc6a81420950a5104e0ec36f4
amir-398 <amir.398@hotmail.fr>
2026-03-24 00:25:18 +0100

    feat(SCRUM-584): replace `expo-blur` with `@sbaiahmed1/react-native-blur` for blur effects and adjust blur intensity.

--- src/features/chat-room/components/chat-room-header/chat-room-header.component.tsx
+++ src/features/chat-room/components/chat-room-header/chat-room-header.component.tsx
@@ -1,9 +1,9 @@
-import { BlurView } from 'expo-blur';
 import { useRouter } from 'expo-router';
 import { ImageSource } from 'expo-image';
 import { StrictOmit } from '@chillui/ui';
 import { StyleSheet } from 'react-native';
 import { useShallow } from 'zustand/react/shallow';
+import { BlurView } from '@sbaiahmed1/react-native-blur';
 import { Avatar, Box, BoxGrow, Icon, String, Wrapper } from '@ludo/ui';
 
 import { serialize } from '@/utils/json.utils';
@@ -16,21 +16,19 @@ import { SessionCollectionItemDtoSport } from '@/api/generated/model';
 
 import { useChatRoomStore } from '../../store/chat-room.store';
 
-
 const styles = StyleSheet.create({
   shadow: {
     boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
   },
 });
 
-
 type LocalSearchParamsPrivateInfoChatRoom = RootStackParamList[typeof ROUTES.CHAT_ROOM.INFO_PRIVATE];
 type LocalSearchParamsSessionInfoChatRoom = RootStackParamList[typeof ROUTES.CHAT_ROOM.INFO_SESSION];
-const BLUR_INTENSITY = 70;
+const BLUR_INTENSITY = 50;
 
 export default function ChatRoomHeader() {
   const router = useRouter();
-  const { top } = useSafeArea()
+  const { top } = useSafeArea();
   const { chatRoomId, imageUrl, name, receiver, sessionSport, sessionUid, type } = useChatRoomStore(
     useShallow(state => ({
       chatRoomId: state.chatRoomId,
@@ -39,17 +37,17 @@ export default function ChatRoomHeader() {
       receiver: state.chatRoomInfo?.receiver,
       sessionSport: state.chatRoomInfo?.sessionData?.sport,
       sessionUid: state.chatRoomInfo?.sessionData?.sessionUid,
-      type: state.chatRoomInfo?.type
-    }))
-  )
-  const { firstname, lastname } = receiver || {}
+      type: state.chatRoomInfo?.type,
+    })),
+  );
+  const { firstname, lastname } = receiver || {};
 
-
-  const chatRoomIsGroup = type !== "PRIVATE"
+  const chatRoomIsGroup = type !== 'PRIVATE';
 
   const getAvatarImage = (): ImageSource | undefined => {
     if (imageUrl) return { uri: imageUrl };
-    if (chatRoomIsGroup && sessionSport) return getSportPlaceHolder(sessionSport as SessionCollectionItemDtoSport) as ImageSource;
+    if (chatRoomIsGroup && sessionSport)
+      return getSportPlaceHolder(sessionSport as SessionCollectionItemDtoSport) as ImageSource;
     return undefined;
   };
   const avatarImage = getAvatarImage();
@@ -61,63 +59,68 @@ export default function ChatRoomHeader() {
         imageUrl,
         name,
         sessionUid,
-      }
+      };
       router.navigate({ params, pathname: ROUTES.CHAT_ROOM.INFO_SESSION });
     } else {
-      const params: StrictOmit<LocalSearchParamsPrivateInfoChatRoom, "chatRoomId"> = {
+      const params: StrictOmit<LocalSearchParamsPrivateInfoChatRoom, 'chatRoomId'> = {
         imageUrl,
         name,
         receiver: serialize(receiver),
-      }
+      };
       router.navigate({
         params,
-        pathname: ROUTES.CHAT_ROOM.INFO_PRIVATE_UID(chatRoomId)
+        pathname: ROUTES.CHAT_ROOM.INFO_PRIVATE_UID(chatRoomId),
       });
     }
-  }
-
+  };
 
   return (
-    <BlurView
-      intensity={BLUR_INTENSITY}
-      experimentalBlurMethod="dimezisBlurView"
-    >
-      <Box style={[styles.shadow, { paddingTop: top }]}>
-        <Wrapper className="flex-row items-center justify-between py-2">
-          <BoxGrow className="flex-row items-center gap-1">
-            <Icon
-              name="arrow-left-regular"
-              size="lg"
-              color={COLORS.muted}
-              onPress={() => {
-                router.back();
+    <Box style={[styles.shadow, { paddingTop: top }]}>
+      <BlurView
+        blurType="light"
+        blurAmount={BLUR_INTENSITY}
+        style={{ bottom: 0, left: 0, position: 'absolute', right: 0, top: 0 }}
+      />
+      <Wrapper className="flex-row items-center justify-between py-2">
+        <BoxGrow className="flex-row items-center gap-1">
+          <Icon
+            name="arrow-left-regular"
+            size="lg"
+            color={COLORS.muted}
+            onPress={() => {
+              router.back();
+            }}
+            pressEffectSize="xs"
+          />
+          {chatRoomIsGroup ? (
+            <Avatar
+              data={{
+                firstname: name,
+                imageUrl: avatarImage,
               }}
-              pressEffectSize="xs"
             />
-            {
-              chatRoomIsGroup ? (
-                <Avatar
-                  data={{
-                    firstname: name,
-                    imageUrl: avatarImage,
-                  }}
-                />
-              ) : (
-                <Avatar
-                  data={{
-                    firstname,
-                    imageUrl: avatarImage,
-                    lastname,
-                  }}
-                />
-              )
-            }
-            <String className="ml-2" colorVariant="muted" font="primaryBold" truncate>
-              {name}
-            </String>
-          </BoxGrow>
-          <Icon name="info-circle-regular" size="lg" color={COLORS.muted} onPress={handleInfoPress} pressEffectSize="xs" className='ml-3' />
-        </Wrapper>
-      </Box></BlurView>
+          ) : (
+            <Avatar
+              data={{
+                firstname,
+                imageUrl: avatarImage,
+                lastname,
+              }}
+            />
+          )}
+          <String className="ml-2" colorVariant="muted" font="primaryBold" truncate>
+            {name}
+          </String>
+        </BoxGrow>
+        <Icon
+          name="info-circle-regular"
+          size="lg"
+          color={COLORS.muted}
+          onPress={handleInfoPress}
+          pressEffectSize="xs"
+          className="ml-3"
+        />
+      </Wrapper>
+    </Box>
   );
 }

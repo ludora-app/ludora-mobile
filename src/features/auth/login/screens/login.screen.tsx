a4cfd4a542973bbeaabe74f10825f234fde298a6
Amir <76406446+amir-398@users.noreply.github.com>
2026-03-24 01:20:02 +0100

    refactor(SCRUM-582): dynamically apply bottom safe area padding using `useSafeArea` hook in login and register screens. (#102)

--- src/features/auth/login/screens/login.screen.tsx
+++ src/features/auth/login/screens/login.screen.tsx
@@ -2,6 +2,7 @@ import { Pressable } from 'react-native';
 import { useTranslate } from '@tolgee/react';
 import { String, Separator, Link, WrapperKeyboardAwareScrollView, BoxGrow } from '@ludo/ui';
 
+import { useSafeArea } from '@/hooks/safe-area.hook';
 import HeaderGoBack from '@/components/ui/navigation/header-go-back/components/header-go-back.component';
 
 import LoginEmailForm from '../components/login-email-form.component';
@@ -9,12 +10,17 @@ import LoginSocialNetworks from '../components/login-social-networks/login-socia
 
 export default function LoginScreen() {
   const { t } = useTranslate();
+  const { bottom } = useSafeArea();
 
   return (
     <>
       <HeaderGoBack title={t('auth.login.header_title')} hasTopSafeArea />
-      <WrapperKeyboardAwareScrollView hasSafeArea edges={['bottom']} contentContainerClassName='grow'>
-        <BoxGrow className='mt-10 pb-5'>
+      <WrapperKeyboardAwareScrollView
+        androidSafeAreaBottom={false}
+        contentContainerClassName="grow"
+        style={{ paddingBottom: bottom }}
+      >
+        <BoxGrow className="mt-10 pb-5">
           <LoginEmailForm />
           <Separator title={t('common.or')} className="my-5" />
           <LoginSocialNetworks flow="login" />

5d1d22430f0639b05640e00b54397fe4e5c4f141
amir-398 <amir.398@hotmail.fr>
2026-03-29 22:23:26 +0200

    fix(SCRUM-596): update profil-header layout spacing

--- src/features/profil/components/profil-header/profil-header.component.tsx
+++ src/features/profil/components/profil-header/profil-header.component.tsx
@@ -1,28 +1,28 @@
-import { useTranslate } from '@tolgee/react'
-import { Box, BoxRowGrow, cn, OutlinedString, Wrapper } from '@chillui/ui'
+import { useTranslate } from '@tolgee/react';
+import { Box, BoxRowGrow, cn, OutlinedString, Wrapper } from '@chillui/ui';
 
-import COLORS from '@/constants/colors.contstants'
-import { useSafeArea } from '@/hooks/safe-area.hook'
-import GoBackButton from '@/components/ui/navigation/header-go-back/components/go-back-button.component'
+import COLORS from '@/constants/colors.contstants';
+import { useSafeArea } from '@/hooks/safe-area.hook';
+import GoBackButton from '@/components/ui/navigation/header-go-back/components/go-back-button.component';
 
-import ProfilHeaderActionsMe from './profil-header-actions-me.component'
-import ProfilHeaderActions from './profil-header-actions/profil-header-actions.component'
+import ProfilHeaderActionsMe from './profil-header-actions-me.component';
+import ProfilHeaderActions from './profil-header-actions/profil-header-actions.component';
 
 interface ProfilHeaderProps {
-  isMe: boolean
-  lastname: string
-  firstname: string
+  isMe: boolean;
+  lastname: string;
+  firstname: string;
 }
 
 export default function ProfilHeader(props: ProfilHeaderProps) {
-  const { t } = useTranslate()
-  const { firstname, isMe: isProfilMe, lastname } = props
+  const { t } = useTranslate();
+  const { firstname, isMe: isProfilMe, lastname } = props;
 
-  const { safeTop } = useSafeArea()
+  const { safeTop } = useSafeArea();
 
   return (
-    <Wrapper className='flex-row items-center z-10 pb-2' style={{ paddingTop: safeTop }}>
-      <BoxRowGrow className='items-center gap-3'>
+    <Wrapper className="z-10 flex-row items-center pb-4" style={{ paddingTop: safeTop }}>
+      <BoxRowGrow className="items-center gap-3">
         {!isProfilMe && <GoBackButton />}
         <Box className={cn({ 'mt-2': !isProfilMe })}>
           <OutlinedString
@@ -37,6 +37,6 @@ export default function ProfilHeader(props: ProfilHeaderProps) {
       </BoxRowGrow>
       <ProfilHeaderActionsMe isMe={isProfilMe} />
       <ProfilHeaderActions isMe={isProfilMe} firstname={firstname} lastname={lastname} />
-    </Wrapper >
-  )
+    </Wrapper>
+  );
 }

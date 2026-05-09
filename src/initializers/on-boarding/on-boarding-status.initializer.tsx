27f2c20f2a50c8ef35a1246cd972046b4d9b8d75
Amir <76406446+amir-398@users.noreply.github.com>
2026-05-10 00:49:41 +0200

    fix(SCRUM-647): update on-boarding initializer to conditionally fetch user data based on authentication status (#156)
    Co-authored-by: amir-398 <amir.398@hotmail.fr>

--- src/initializers/on-boarding/on-boarding-status.initializer.tsx
+++ src/initializers/on-boarding/on-boarding-status.initializer.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 
+import { useAuthStore } from '@/stores/auth.store';
 import { useUserMe } from '@/queries/user-me.query';
 import { useOnBoardingStatusStore } from '@/stores/on-boarding-status.store';
 
 export default function OnBoardingStatusInitializer() {
-  const { userMe } = useUserMe();
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const { userMe } = useUserMe(isAuthenticated);
   const setOnBoardingStatus = useOnBoardingStatusStore(state => state.setOnBoardingStatus);
   const { onBoardingStatus: userOnBoardingStatus } = userMe || {};
-
 
   useEffect(() => {
     if (userOnBoardingStatus) {
